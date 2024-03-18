c53cca7d506ea8ea0b3b02d5849d43dbba50c9cd
ruoyu1123 <729800244@qq.com>
2024-03-18 10:34:55 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Geva',
+    version='1.0.0',
+    author='yangjinbao',
+    author_email='ruoyu1123@outlook.com',
+    description='A genomic assement tools including Completeness and Accuracy',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/panlab-bioinfo/Geva.git',
+    packages=find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        'pandas',
+        # Add any other dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
